ee0329b3453cb66399155a69146d93ee9f1ba23b
user <daniil.stepanenko.96@gmail.com>
2020-03-10 21:37:10 +0200

    add service

---
+++ app/usecases/manga.go
@@ -0,0 +1,20 @@
+package usecases
+
+import (
+	"github.com/allpigsmustdie/mangago/app/domain/models"
+	"github.com/allpigsmustdie/mangago/app/domain/repository"
+)
+
+type MangaService struct {
+	repo repository.Manga
+}
+
+func (m *MangaService) Create(manga models.Manga) (id int, err error) {
+	// TODO: validation
+	return m.repo.Create(manga)
+}
+
+func (m *MangaService) Get(id int) (*models.Manga, error) {
+	return m.repo.Get(id)
+}
+
---
+++ app/usecases/manga_test.go
@@ -0,0 +1,5 @@
+package usecases
+
+import "github.com/allpigsmustdie/mangago/app/domain/service"
+
+var _ service.Manga = new(MangaService)
