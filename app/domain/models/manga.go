b7722c5d198a47689f9e16b564c8253cfc5b4737
user <daniil.stepanenko.96@gmail.com>
2020-03-10 23:01:55 +0200

    remove id from model

--- app/domain/models/manga.go
+++ app/domain/models/manga.go
@@ -3,13 +3,12 @@ package models
 import "time"
 
 type Manga struct {
-	Id int
 	Name string
 	Author string
 	ReleaseDate time.Time
 	Description string
 }
 
-func NewManga(id int, name string, author string, releaseDate time.Time, description string) *Manga {
-	return &Manga{Id: id, Name: name, Author: author, ReleaseDate: releaseDate, Description: description}
+func NewManga(name string, author string, releaseDate time.Time, description string) Manga {
+	return Manga{Name: name, Author: author, ReleaseDate: releaseDate, Description: description}
 }
